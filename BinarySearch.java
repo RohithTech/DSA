bb54edb18c843e9126dbab00f1cd774c0437f5e0
Rohith <Rohithkumar2196@gmail.com>
2025-10-24 11:59:54 +0530

    Refactor BinarySearch.java for clarity
    Removed commented line and adjusted formatting.

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,5 +1,5 @@
 package DSA;
-// this is to understand the merge from Rusith
+
 public class BinarySearch {
     public static int binarySearch(int[] arr,int target){
         int left = 0;
@@ -19,9 +19,8 @@ public class BinarySearch {
         int[] array={10,20,30,40,50,60,70,80};
         int tar_value =50;
         int result =binarySearch(array, tar_value);
-        if(result==-1){
+        if(result==-1)
         System.out.println("The element is not found");
-        System.out.println("Thankyou Rusi"); }
         else
          System.out.println("The element found at :"+result);
     }
