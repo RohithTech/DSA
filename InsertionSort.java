8895099331d169de09d67dd6b7a4b8dd1142edc6
Rohith <Rohithkumar2196@gmail.com>
2025-11-17 13:21:57 +0530

    Update InsertionSort.java

--- InsertionSort.java
+++ InsertionSort.java
@@ -2,6 +2,6 @@ package DSA;
 
 public class InsertionSort {
     public static void main(String[] args) {
-    System.out .println("This is insertion sort");
+    System.out .println("This is Insertion Sort");
     }
 }
